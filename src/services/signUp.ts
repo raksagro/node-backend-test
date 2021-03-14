57f34310bc7768d98a54a1d26dfaa9e77131e583
ETomacheski <tomacheski2001@gmail.com>
2021-03-14 17:47:52 -0300

    feat: ensure Singup return 500 if sequelize returns throws

--- src/services/signUp.ts
+++ src/services/signUp.ts
@@ -5,16 +5,23 @@ import { HttpResponse } from "../protocols/HttpResponse-protocol";
 export class SingUpService{
   
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
     if (!httpRequest.body.name) return {
       statusCode: 400,
       body: new Error('Missing param error: Name')
     }
     
-    const user = await User.create(httpRequest.body).catch(console.log)
+    const user = await User.create(httpRequest.body)
 
     return {
       statusCode: 200,
       body: user
     }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err
+    }
+  }
   } 
 }
