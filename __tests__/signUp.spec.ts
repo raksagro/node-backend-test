1f7b09469e5479abe68c3a2676d20943bdd29e57
ETomacheski <tomacheski2001@gmail.com>
2021-03-14 17:48:28 -0300

    test: ensure Singup return 500 if sequelize returns throws

--- __tests__/signUp.spec.ts
+++ __tests__/signUp.spec.ts
@@ -1,4 +1,6 @@
+import User from "../src/models/user";
 import { SingUpService } from "../src/services/signUp";
+
 
 describe('SignUp Service', () => {
   test('Should return 400 if no name is provided ', async () => {
@@ -19,6 +21,7 @@ describe('SignUp Service', () => {
   test('Should return 200 valid data is provided',async () => {
     const sut = new SingUpService()
 
+
     const userData = {
       body: {
         name: 'any_name',
@@ -32,5 +35,24 @@ describe('SignUp Service', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body.id).toBeTruthy()
+  })
+  test('Should return 500 if sequelize throws',async () => {
+    const sut = new SingUpService()
+    
+    jest.spyOn(User, 'create').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+
+    const userData = {
+      body: {
+        name: 'any_name',
+        dob: 'any_dob',                    
+        address: 'any_address',                
+        description: 'any_description', 
+      }           
+    }
+
+    const response = await sut.handle(userData)
+
+    expect(response.statusCode).toBe(500)
   })
 })
