70b36d49434844f06ab18c1a84595b58a774fa00
ETomacheski <tomacheski2001@gmail.com>
2021-03-14 22:21:53 -0300

    feat: add endPoints access

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { UserController } from './controller/user'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('hello word')
-})
+const userController = new UserController
+
+router.get('/user/:id', userController.get)
+router.post('/user', userController.create)
+router.put('/user', userController.update)
+router.delete('/user/:id', userController.delete)
+
+
+
 
 export default router
