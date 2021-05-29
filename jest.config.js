0b041e3bec31e3a0b0e26fff6deaa9f75740a0e2
Gustavo Farias <gustavo.farias@paipe.co>
2021-05-29 20:54:53 -0300

    chore: directories structure setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+import { resolve } from 'path';
+const root = resolve(__dirname);
+export const rootDir = root;
+export const displayName = 'root-tests';
+export const testMatch = ['<rootDir>/src/**/*.test.ts'];
+export const testEnvironment = 'node';
+export const clearMocks = true;
+export const preset = 'ts-jest';
+export const moduleNameMapper = {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+};
