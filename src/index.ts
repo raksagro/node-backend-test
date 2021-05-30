033c75254fb3da138de33b523309d330ea045653
Gustavo Farias <gustavo.farias@paipe.co>
2021-05-29 22:34:37 -0300

    refactor(server): setup server port by config property

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import 'module-alias/register';
 import { SetupServer } from './main/server';
+import config from 'config';
 
 (async (): Promise<void> => {
   try {
-    const server = new SetupServer(8000);
+    const server = new SetupServer(config.get('App.port'));
     await server.init();
     server.start();
   } catch (error) {
