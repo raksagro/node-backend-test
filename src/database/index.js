99341e498bbc518a358c1983221ee53e8c09a2a4
Nadine-Schneider <nadine_schneider@hotmail.com>
2021-07-11 15:17:48 -0300

    add files app and model index [2]

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/config');
+const dbConfig = require('../config/database');
 const User = require('../models/user');
 
 const connection = new Sequelize(dbConfig);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
-const express = require("express");
-const routes = require('./routes');
+const app = require('./app');
 require("dotenv").config();
-require('./database');
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(routes);
 
 app.listen(port, () => console.log("Server is running on port", + port));
